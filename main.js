5a49b943df66517fb33bc8e7527af9c7b958686b
Shashi Singh <singh.shashishekhar@gmail.com>
2018-06-08 19:16:50 +0530

    Repro bug where double clicking on disabled maximize button, maximizes the window

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
+  mainWindow.setMaximizable(false);
+
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
